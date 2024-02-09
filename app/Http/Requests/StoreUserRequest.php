63e3a3e5e1d8524c8caf572d8de830560ca63ee9
unknown <antoine.cuvilliez@outlook.fr>
2024-02-09 16:41:41 +0100

    fix(database) Changement table users

---
+++ app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "nom" => "required|string|max:30",
+            "prenom" => "required|string|max:30",
+            "pseudonyme" => "required|string|max:30",
+            "email" => "required|email|max:100",
+            "password" => "required|string|max:255",
+        ];
+    }
+}
