04c6d6722326092e6264df6bcf186343a05d000d
unknown <antoine.cuvilliez@outlook.fr>
2024-04-04 18:35:05 +0200

    feat(authentification) Ajout lien prod VerifyCsrfToken

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -15,5 +15,7 @@ class VerifyCsrfToken extends Middleware
         'http://127.0.0.1:8000/register',
         'http://127.0.0.1:8000/login',
         'http://127.0.0.1:8000/logout',
+        
+        'https://projet-resources.fr/login'
     ];
 }
