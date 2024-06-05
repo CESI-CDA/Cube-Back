62efeed0948f649f7198ee4e2f7522e89c4ed4d7
unknown <antoine.cuvilliez@outlook.fr>
2024-06-05 09:28:47 +0200

    feat(commentaire) Ajout getUtilisateur

--- app/Models/LienRessourceCommentaire.php
+++ app/Models/LienRessourceCommentaire.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LienRessourceCommentaire extends Model
 {
@@ -12,4 +13,9 @@ class LienRessourceCommentaire extends Model
     protected $table = 'lien_ressource_commentaire';
     protected $fillable = ['id_res', 'id_user', 'date', 'commentaire', 'id_commentaire_parent', 'id_etat', 'deleted'];
     public $timestamps = true;
+
+    public function getUtilisateur(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'id_user')->where('deleted', false)->select(['id', 'nom', 'prenom', 'pseudonyme']);
+    }
 }
