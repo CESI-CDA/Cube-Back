3cc1358ba072a117c496c0ea677745ff108e1723
unknown <antoine.cuvilliez@outlook.fr>
2024-09-10 15:47:28 +0200

    feat(test) Ajout et refactor test

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -3,24 +3,23 @@
 namespace Tests\Feature\Auth;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class AuthenticationTest extends TestCase
 {
-    use RefreshDatabase;
-
+    use DatabaseTransactions;
+    
     public function test_users_can_authenticate_using_the_login_screen(): void
     {
         $user = User::factory()->create();
 
-        $response = $this->post('/login', [
+        $this->post('/api/login', [
             'email' => $user->email,
             'password' => 'password',
         ]);
 
         $this->assertAuthenticated();
-        $response->assertNoContent();
     }
 
     public function test_users_can_not_authenticate_with_invalid_password(): void
@@ -39,9 +38,8 @@ class AuthenticationTest extends TestCase
     {
         $user = User::factory()->create();
 
-        $response = $this->actingAs($user)->post('/logout');
+        $this->actingAs($user)->post('/api/logout');
 
         $this->assertGuest();
-        $response->assertNoContent();
     }
 }
--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -2,23 +2,26 @@
 
 namespace Tests\Feature\Auth;
 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class RegistrationTest extends TestCase
 {
-    use RefreshDatabase;
-
+    use DatabaseTransactions;
+    
     public function test_new_users_can_register(): void
     {
-        $response = $this->post('/register', [
-            'name' => 'Test User',
+        $this->post('/api/register', [
+            'nom' => 'Test',
+            'prenom' => 'User',
+            'pseudonyme' => 'UserTest',
             'email' => 'test@example.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
+            'password' => 'LaravelDev123!',
+            'password_confirmation' => 'LaravelDev123!',
+            'id_rol' => 4
         ]);
 
         $this->assertAuthenticated();
-        $response->assertNoContent();
     }
 }
